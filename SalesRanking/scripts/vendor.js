b8e7ba01b26ffbf68e152fad9f9f21c93117a31d
Eduardo Pinto <rumbafum@gmail.com>
2013-09-02 09:20:26 -0500

    Changes made by Eduardo Pinto (rumbafum@gmail.com).

--- SalesRanking/scripts/vendor.js
+++ SalesRanking/scripts/vendor.js
@@ -25,7 +25,6 @@
         vendorTotalEarned: "",
         vendorObjective: "",
         vendorImage: "",
-        lastMonthVariation: "",
         
         init: function () {
             var that = this;
