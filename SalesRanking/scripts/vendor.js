a2c00b2c2a4040b6f17a063800e0512576ae8631
Eduardo Pinto <rumbafum@gmail.com>
2013-09-02 06:29:59 -0500

    Adicionada informação à view do vendedor

--- SalesRanking/scripts/vendor.js
+++ SalesRanking/scripts/vendor.js
@@ -19,6 +19,11 @@
     VendorViewModel = kendo.data.ObservableObject.extend({
         
         vendorsDataSource: null,
+        lastMonthGrowthDataSourceByClient: null,
+        vendorName: "",
+        vendorBestClient: "",
+        vendorTotalEarned: "",
+        vendorObjective: "",
         
         init: function () {
             var that = this;
@@ -29,16 +34,104 @@
             var view = e.view;
         },
         
+        createLastMonthGrowthChart: function() {
+            app.vendorService.viewModel.drawLastMonthGrowthChart();
+            app.vendorService.viewModel.bindResizeEvent();  
+        },
+        
+        drawLastMonthGrowthChart: function() {
+            var $clientsColumnChart = $("#vendorclientscolumn-chart");
+
+            clientsColumnChart = $clientsColumnChart.kendoChart({
+                dataSource: this.lastMonthGrowthDataSourceByClient,
+                theme: app.chartsTheme,
+                renderAs: "svg",
+                title: {
+                    position: "top",
+                    text: "Crescimento Clientes Último Mês"
+                },
+                legend: {
+                    position: "bottom"
+                },
+                chartArea: {
+                    background: ""
+                },
+                seriesDefaults: {
+                    type: "column"
+                },
+                series: [
+                    {
+                        field: "Growth",
+                        colorField: "UserColor"
+                    }
+                ],
+                categoryAxis: {
+                    field: "Clients",
+                    majorGridLines: {
+                        visible: false
+                    },
+                    labels:{
+                        template: '#: data.value.toString().length > 25 ? data.value.substring(0,25) + "..." : data.value #',
+                        rotation: -90
+                    }
+                },
+                valueAxis: {
+                    labels: {
+                        template: '#: data.value # %'
+                    },
+                    line: {
+                        visible: false
+                    }
+                },
+                tooltip: {
+                    visible: true,
+                    template: kendo.template('#: value #% Ranking clientes:#: dataItem.Ranking #º')
+                },
+                
+            }).data("kendoChart");
+        },
+        
+        bindResizeEvent: function () {
+            $(window).on("resize.clientsColumnChart", $.proxy(app.vendorService.viewModel.drawLastMonthGrowthChart, app.vendorService.viewModel));
+        },
+
+        unbindResizeEvent: function () {
+            $(window).off("resize.clientsColumnChart");
+        },
+        
         loadData: function(e) {
             try{
                 e.view.loader.show();
                 var uid = e.view.params.uid;
-                if(uid)
-                    e.view.header.find('.view-title-container .viewTitle').text(uid);
-                
+                if(!uid && app.vendorService.viewModel.vendorName)
+                    uid = app.vendorService.viewModel.vendorName;
+                if(!uid)
+                    uid = "Abertura De Clientes";
+                e.view.header.find('.view-title-container .viewTitle').text(uid);
+                var dataSource = new kendo.data.DataSource({
+                    transport: {
+                        read: {
+                            url: "http://localhost/SalesRankingWeb/api/Vendors/Details?json=true&vendorName=" + uid,
+                            dataType: "json"
+                        }
+                    },
+                    change: function() {
+                        app.vendorService.viewModel.vendorName = this.data()[0].Name;
+                        app.vendorService.viewModel.vendorBestClient = "Melhor Cliente: " + this.data()[0].BestClient;
+                        app.vendorService.viewModel.vendorTotalEarned = "Total Vendas anuais: " + this.data()[0].TotalSales;
+                        app.vendorService.viewModel.vendorObjective = "Objectivo anual: " + this.data()[0].TotalTarget;
+                        kendo.bind($(".vendorInfo .spanVendorName"), app.vendorService.viewModel);
+                        kendo.bind($(".vendorInfo .spanVendorBestClient"), app.vendorService.viewModel);
+                        kendo.bind($(".vendorInfo .spanVendorTotalEarned"), app.vendorService.viewModel);
+                        kendo.bind($(".vendorInfo .spanVendorObjective"), app.vendorService.viewModel);
+                        getData(6, "http://localhost/SalesRankingWeb/api/Vendors/GetVendorLastMonthGrowthRankingByClient?vendorName=" + this.data()[0].Name,
+                            "lastMonthGrowthDataSourceByClient", app.vendorService.viewModel);                        
+                        app.vendorService.viewModel.createLastMonthGrowthChart();
+                    }
+                });
+                dataSource.read();
             }
             catch(exc){
-                   
             }
             finally{
                 e.view.loader.hide();
@@ -67,13 +160,7 @@
                             url: "data/VendorList.json",//"http://localhost/SalesRankingWeb/api/Vendors/List?json=true",
                             dataType: "json"
                         }
-                    },
-                    change: function() {
-                        /*var data = this.data();
-                        for(var i=0; i< data.length; i++){
-                        
-                        }*/
-                    }   
+                    } 
                 });
                 dataSource.read();
                 $("#vendorFilterListView").kendoMobileListView({
